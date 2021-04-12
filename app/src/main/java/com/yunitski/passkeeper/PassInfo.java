f4afbb15f7a41921daff6df00d63eaef23137404
YuriYunitski <Lindio@yandex.ru>
2021-04-12 16:07:00 +0300

    upp

--- app/src/main/java/com/yunitski/passkeeper/PassInfo.java
+++ app/src/main/java/com/yunitski/passkeeper/PassInfo.java
@@ -2,22 +2,28 @@ package com.yunitski.passkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PassInfo extends AppCompatActivity {
+public class PassInfo extends AppCompatActivity implements View.OnClickListener {
 
-    EditText res, link, pass;
+    TextView res, link, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_info);
 
-        res = findViewById(R.id.etResInfo);
-        link = findViewById(R.id.etLinkInfo);
-        pass = findViewById(R.id.etPassInfo);
+        res = findViewById(R.id.tvResInfo);
+        link = findViewById(R.id.tvLinkInfo);
+        pass = findViewById(R.id.tvPassInfo);
 
         Intent intent = getIntent();
         String r = intent.getStringExtra("resIn");
@@ -26,5 +32,38 @@ public class PassInfo extends AppCompatActivity {
         res.setText(r);
         link.setText(l);
         pass.setText(p);
+        res.setClickable(true);
+        link.setClickable(true);
+        pass.setClickable(true);
+        res.setOnClickListener(this);
+        link.setOnClickListener(this);
+        pass.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvResInfo:
+                String s = res.getText().toString();
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("copied_text", s);
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(this, "Скопировано", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvLinkInfo:
+                String s2 = link.getText().toString();
+                ClipboardManager clipboard2 = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip2 = ClipData.newPlainText("copied_text", s2);
+                clipboard2.setPrimaryClip(clip2);
+                Toast.makeText(this, "Скопировано", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvPassInfo:
+                String s3 = pass.getText().toString();
+                ClipboardManager clipboard3 = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip3 = ClipData.newPlainText("copied_text", s3);
+                clipboard3.setPrimaryClip(clip3);
+                Toast.makeText(this, "Скопировано", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
